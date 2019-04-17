6536e6cf7d9d794fffb517e7c36061e3c76f0196
Yomguithereal <guillaumeplique@gmail.com>
2019-04-17 18:01:58 +0200

    Int total

--- motel/cli/__main__.py
+++ motel/cli/__main__.py
@@ -18,10 +18,28 @@ def main():
     subparsers = parser.add_subparsers(help='action to execute', title='actions', dest='action')
 
     preprocess = subparsers.add_parser('preprocess', description='Preprocess the given corpus by splitting sentences and filtering tokens etc.')
-    preprocess.add_argument('column', help='column')
-    preprocess.add_argument('file', help='csv file to cluster', type=FileType('r'), default=sys.stdin, nargs='?')
-    preprocess.add_argument('-o', '--output', help='output file', type=FileType('w'), default=sys.stdout)
-    preprocess.add_argument('-t', '--total', help='Total number of documents. Necessary if you want to display a finite progress indicator.')
+    preprocess.add_argument(
+        'column',
+        help='column'
+    )
+    preprocess.add_argument(
+        'file',
+        help='csv file to cluster',
+        type=FileType('r'),
+        default=sys.stdin,
+        nargs='?'
+    )
+    preprocess.add_argument('-o',
+        '--output',
+        help='output file',
+        type=FileType('w'),
+        default=sys.stdout
+    )
+    preprocess.add_argument('-t',
+        '--total',
+        help='Total number of documents. Necessary if you want to display a finite progress indicator.',
+        type=int
+    )
     SUBPARSERS['preprocess'] = preprocess
 
     args = parser.parse_args()
